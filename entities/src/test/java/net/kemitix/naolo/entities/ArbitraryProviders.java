78b6579eff0b8b1d038a559888bce790e76659a7
Paul Campbell <pcampbell@kemitix.net>
2018-07-30 15:06:37 +0100

    Make ArbitraryProviders public and add javadoc

--- entities/src/test/java/net/kemitix/naolo/entities/ArbitraryProviders.java
+++ entities/src/test/java/net/kemitix/naolo/entities/ArbitraryProviders.java
@@ -8,9 +8,24 @@ import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.temporal.ChronoUnit;
 
-class ArbitraryProviders {
+/**
+ * Providers of Arbitrary values for use with jqwik property based tests.
+ *
+ * @author Paul Campbell (pcampbell@kemitix.net)
+ */
+public class ArbitraryProviders {
 
-    static Arbitrary<ZonedDateTime> zonedDateTime() {
+    /**
+     * Generator of Arbitrary ZonedDataTime values.
+     *
+     * <p>This implementation uses values generated from {@link Instant} which have a slightly larger value range than
+     * {@link ZonedDateTime}. The range for {@link Instant} extends one year before and and after that of
+     * {@link ZonedDateTime}. The range of {@link Instant} values used is therefor reduced to fit within that of
+     * {@link ZonedDateTime}.</p>
+     *
+     * @return an Arbitrary value generator for ZonedDateTime values
+     */
+    public static Arbitrary<ZonedDateTime> zonedDateTime() {
         // Instant supports a range a year before and after that supported by ZonedDateTime
         final Instant minInstantAllowed = Instant.MIN.plus(367, ChronoUnit.DAYS);
         final Instant maxInstantAllowed = Instant.MAX.minus(367, ChronoUnit.DAYS);
