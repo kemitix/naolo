4e7d259710c44e4643884216956e6d16b06961fd
Paul Campbell <pcampbell@kemitix.net>
2020-04-11 19:27:45 +0100

    core.AddPet: added

---
+++ core/src/main/java/net/kemitix/naolo/core/pets/AddPet.java
@@ -0,0 +1,19 @@
+package net.kemitix.naolo.core.pets;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import net.kemitix.naolo.core.AddEntityUseCase;
+import net.kemitix.naolo.entities.Pet;
+import net.kemitix.naolo.storage.spi.EntityRepository;
+
+import javax.enterprise.context.Dependent;
+
+@Dependent
+@RequiredArgsConstructor
+public class AddPet
+        implements AddEntityUseCase<Pet> {
+
+    @Getter
+    private final EntityRepository<Pet> repository;
+
+}
