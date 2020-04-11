9e4f0f50f3773950bc283ce2763ea91db49ce00e
Paul Campbell <pcampbell@kemitix.net>
2020-04-11 20:17:14 +0100

    storage-spi.EntityRepository made base for {Vet*,Owner}Repository

--- storage-spi/src/main/java/net/kemitix/naolo/storage/spi/OwnerRepository.java
+++ storage-spi/src/main/java/net/kemitix/naolo/storage/spi/OwnerRepository.java
@@ -2,17 +2,11 @@ package net.kemitix.naolo.storage.spi;
 
 import net.kemitix.naolo.entities.Owner;
 
-import java.util.Optional;
-import java.util.stream.Stream;
+/**
+ * @deprecated Use {@link EntityRepository<Owner>}
+ */
+@Deprecated
+public interface OwnerRepository
+        extends EntityRepository<Owner>{
 
-public interface OwnerRepository {
-    Owner add(Owner owner);
-
-    Optional<Owner> find(long id);
-
-    Stream<Owner> findAll();
-
-    Optional<Owner> remove(long id);
-
-    Optional<Owner> update(Owner owner);
 }
