a976c7832237b23507bcc5bbb71ad32e229c82e2
Paul Campbell <pcampbell@kemitix.net>
2020-04-13 17:50:22 +0100

    api-tests: extract AbstractIT

---
+++ api-tests/src/test/java/net/kemitix/naolo/api/AbstractIT.java
@@ -0,0 +1,166 @@
+package net.kemitix.naolo.api;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.ValidatableResponse;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
+import org.apache.http.HttpHeaders;
+import org.hamcrest.Matcher;
+
+import java.net.URI;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+
+public class AbstractIT {
+    protected ValidatableResponse deleteOwner(final long id) {
+        return given()
+                .delete("/owners/" + id)
+                .then();
+    }
+
+    protected ValidatableResponse updateOwner(
+            final JsonObject updatedOwner,
+            final long id
+    ) {
+        return given()
+                .contentType(ContentType.JSON)
+                .body(updatedOwner.encode())
+                .put("/owners/" + id)
+                .then();
+    }
+
+    protected ValidatableResponse addOwner(final JsonObject newOwner) {
+        return given()
+                .contentType(ContentType.JSON)
+                .body(newOwner.encode())
+                .post("/owners")
+                .then();
+    }
+
+    protected ValidatableResponse getOwner(final long id) {
+        return given()
+                .get("/owners/" + id)
+                .then();
+    }
+
+    protected ValidatableResponse deletePet(final long id) {
+        return given()
+                .delete("/pets/" + id)
+                .then();
+    }
+
+    protected ValidatableResponse updatePet(
+            final JsonObject pet,
+            final long id
+    ) {
+        return given()
+                .contentType(ContentType.JSON)
+                .body(pet.encode())
+                .put("/pets/" + id)
+                .then();
+    }
+
+    protected ValidatableResponse addPet(final JsonObject pet) {
+        return given()
+                .contentType(ContentType.JSON)
+                .body(pet.encode())
+                .post("/pets")
+                .then();
+    }
+
+    protected ValidatableResponse getPet(final long id) {
+        return given()
+                .get("/pets/" + id)
+                .then();
+    }
+
+    protected Matcher<String> isEmpty() {
+        return is("[]");
+    }
+
+    protected ValidatableResponse listAllOwners() {
+        return given()
+                .get("/owners").then();
+    }
+
+    protected long getIdFromLocationHeader(final ValidatableResponse addResponse) {
+        return Long.parseLong(
+                URI.create(addResponse
+                        .extract()
+                        .header(HttpHeaders.LOCATION))
+                        .getPath()
+                        .split("/")[2]);
+    }
+
+    protected JsonObject createOwner() {
+        return new JsonObject()
+                .put("id", 0)
+                .put("firstName", "Owners First Name")
+                .put("lastName", "Owners Last Name")
+                .put("street", "Owners Street Address")
+                .put("city", "Owners City");
+    }
+
+    protected ValidatableResponse listAllPets() {
+        return given()
+                .get("/pets").then();
+    }
+
+    protected JsonObject createPet(final JsonObject addedOwner) {
+        return new JsonObject()
+                .put("id", 0)
+                .put("name", "Pets Name")
+                .put("dateOfBirth", "2020-03-24")
+                .put("type", "DOG")
+                .put("owner", addedOwner);
+    }
+
+    protected JsonObject createVet() {
+        return new JsonObject()
+                .put("id", 0)
+                .put("name", "Vets Name")
+                .put("specialisations", new JsonArray()
+                        .add("DENTISTRY")
+                        .add("RADIOLOGY"));
+    }
+
+    protected ValidatableResponse deleteVet(final long id) {
+        return given()
+                .when().delete("/vets/" + id)
+                .then();
+    }
+
+    protected ValidatableResponse updateVet(
+            final JsonObject updatedVet,
+            final long id
+    ) {
+        return given()
+                .when()
+                .contentType(ContentType.JSON)
+                .body(updatedVet.encode())
+                .put("/vets/" + id)
+                .then();
+    }
+
+    protected ValidatableResponse getVet(final long id) {
+        return given()
+                .when().get("/vets/" + id)
+                .then();
+    }
+
+    protected ValidatableResponse addVet(final JsonObject newVet) {
+        return given()
+                .when()
+                .contentType(ContentType.JSON)
+                .body(newVet.encode())
+                .post("/vets")
+                .then();
+    }
+
+    protected ValidatableResponse listAllVets() {
+        return given()
+                .when().get("/vets")
+                .then();
+    }
+}
--- api-tests/src/test/java/net/kemitix/naolo/api/PetsIT.java
+++ api-tests/src/test/java/net/kemitix/naolo/api/PetsIT.java
@@ -2,26 +2,19 @@ package net.kemitix.naolo.api;
 
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
-import io.restassured.response.ValidatableResponse;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
-import org.hamcrest.Matcher;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.net.URI;
-
-import static io.restassured.RestAssured.given;
-import static javax.ws.rs.core.HttpHeaders.LOCATION;
 import static net.kemitix.naolo.api.JsonMatcher.jsonArray;
 import static net.kemitix.naolo.api.JsonMatcher.jsonObject;
 import static org.apache.http.HttpStatus.SC_OK;
-import static org.hamcrest.Matchers.is;
-import static org.jboss.resteasy.spi.HttpResponseCodes.SC_CREATED;
 import static org.jboss.resteasy.spi.HttpResponseCodes.SC_NOT_FOUND;
 
 @QuarkusTest
-public class PetsIT {
+public class PetsIT
+        extends AbstractIT {
 
     @Test
     @DisplayName("Pets lifecycle - add - list - update - remove")
@@ -33,52 +26,20 @@ public class PetsIT {
 
         // add a pet
         // first - add the owner
-        final var newOwner = new JsonObject()
-                .put("id", 0)
-                .put("firstName", "Bob")
-                .put("lastName", "Smith")
-                .put("street", "the road")
-                .put("city", "capital");
-        final URI ownerUri =
-                URI.create(given()
-                        .contentType(ContentType.JSON)
-                        .body(newOwner.encode())
-                        .post("/owners")
-                        .then()
-                        .statusCode(SC_CREATED)
-                        .extract()
-                        .header(LOCATION));
-        final String ownerId =
-                ownerUri.getPath()
-                        .split("/")[2];
-        final var addedOwner = newOwner.copy()
-                .put("id", Long.valueOf(ownerId));
+        final var newOwner = createOwner();
+        final long ownerId = getIdFromLocationHeader(addOwner(newOwner));
+        final var addedOwner = newOwner.copy().put("id", ownerId);
         // now add the pet
-        final JsonObject newPet =
-                new JsonObject()
-                        .put("id", 0)
-                        .put("name", "Pets Name")
-                        .put("dateOfBirth", "2020-03-24")
-                        .put("type", "DOG")
-                        .put("owner", addedOwner);
-        final var addResponse =
-                addPet(newPet)
-                        .statusCode(SC_CREATED);
-        final int id = Integer.parseInt(
-                URI.create(addResponse
-                        .extract()
-                        .header(LOCATION))
-                        .getPath()
-                        .split("/")[2]);
-        final JsonObject addedPet=
-                newPet.copy()
-                        .put("id", id);
+        final var newPet = createPet(addedOwner);
+        final long petId = getIdFromLocationHeader(addPet(newPet));
+        final var addedPet= newPet.copy().put("id", petId);
 
         // get the pet
-        getPet(id)
+        getPet(petId)
                 .statusCode(SC_OK)
                 .contentType(ContentType.JSON)
                 .body(jsonObject(addedPet));
+
         // list all pets - we have one
         final JsonArray petList=
                 new JsonArray()
@@ -91,12 +52,12 @@ public class PetsIT {
         final JsonObject updatedPet =
                 addedPet.copy()
                         .put("name", "Pets new name");
-        updatePet(updatedPet, id)
+        updatePet(updatedPet, petId)
                 .statusCode(SC_OK)
                 .body(jsonObject(updatedPet));
 
         // get the updated pet
-        getPet(id)
+        getPet(petId)
                 .statusCode(SC_OK)
                 .body(jsonObject(updatedPet));
 
@@ -109,17 +70,15 @@ public class PetsIT {
                 .body(jsonArray(updatedPetList));
 
         // remove pet
-        deletePet(id)
+        deletePet(petId)
                 .statusCode(SC_OK);
 
         // remove owner
-        given()
-                .delete("/owners/" + ownerId)
-                .then()
+        deleteOwner(ownerId)
                 .statusCode(SC_OK);
 
         // get pet - not found
-        getPet(id)
+        getPet(petId)
                 .statusCode(SC_NOT_FOUND);
 
         // list all pets - none found
@@ -128,43 +87,4 @@ public class PetsIT {
                 .body(isEmpty());
     }
 
-    private ValidatableResponse deletePet(final int id) {
-        return given()
-                .delete("/pets/" + id)
-                .then();
-    }
-
-    private ValidatableResponse updatePet(
-            final JsonObject pet,
-            final int id
-    ) {
-        return given()
-                .contentType(ContentType.JSON)
-                .body(pet.encode())
-                .put("/pets/" + id)
-                .then();
-    }
-
-    private ValidatableResponse addPet(final JsonObject pet) {
-        return given()
-                .contentType(ContentType.JSON)
-                .body(pet.encode())
-                .post("/pets")
-                .then();
-    }
-
-    private ValidatableResponse getPet(final int id) {
-        return given()
-                .get("/pets/" + id)
-                .then();
-    }
-
-    private Matcher<String> isEmpty() {
-        return is("[]");
-    }
-
-    private ValidatableResponse listAllPets() {
-        return given()
-                .get("/pets").then();
-    }
 }
