5c652dc35855658192fa7c806f4407d577742350
Paul Campbell <pcampbell@kemitix.net>
2020-04-19 14:37:47 +0100

    war: add an Application instance

---
+++ war/src/main/java/net/kemitix/naolo/war/NaoloApp.java
@@ -0,0 +1,8 @@
+package net.kemitix.naolo.war;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/naolo")
+public class NaoloApp extends Application {
+}
---
+++ war/src/test/java/net/kemitix/naolo/war/NaoloAppTest.java
@@ -0,0 +1,15 @@
+package net.kemitix.naolo.war;
+
+import org.assertj.core.api.WithAssertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class NaoloAppTest
+        implements WithAssertions {
+
+    @Test
+    @DisplayName("Can Instantiate")
+    public void canInstantiate() {
+        assertThat(new NaoloApp()).isNotNull();
+    }
+}
