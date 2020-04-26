c829169eef7ea7365ce6d0521c6de48d43cb2994
Paul Campbell <pcampbell@kemitix.net>
2020-04-26 09:34:41 +0100

    Don’t block access to resources

--- war/src/main/java/net/kemitix/naolo/war/NaoloApp.java
+++ war/src/main/java/net/kemitix/naolo/war/NaoloApp.java
@@ -3,7 +3,7 @@ package net.kemitix.naolo.war;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-@ApplicationPath("/")
+@ApplicationPath("/api")
 public class NaoloApp extends Application {
 
 }
