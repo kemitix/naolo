5290ee06ec7cc5fee8906fc11bd3f711c13eb90f
Paul Campbell <pcampbell@kemitix.net>
2020-05-02 20:48:58 +0100

    pets: add stub feature and items

---
+++ pets/src/main/java/net/kemitix/naolo/pets/Pets.java
@@ -0,0 +1,18 @@
+package net.kemitix.naolo.pets;
+
+import javax.inject.Qualifier;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.CONSTRUCTOR;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.ElementType.TYPE;
+
+@Qualifier
+@Retention(RetentionPolicy.RUNTIME)
+@Target({TYPE, METHOD, FIELD, PARAMETER, CONSTRUCTOR})
+public @interface Pets {
+}
