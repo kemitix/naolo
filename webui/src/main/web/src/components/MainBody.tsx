e74e3506a4e6ec3c94ac061548e6197e53ac0e88
Paul Campbell <pcampbell@kemitix.net>
2020-05-02 19:38:28 +0100

    webui/MainBody: fix deep linking using HashRouter

--- webui/src/main/web/src/components/MainBody.tsx
+++ webui/src/main/web/src/components/MainBody.tsx
@@ -1,10 +1,5 @@
 import React from "react";
-import {
-    BrowserRouter as Router,
-    Route,
-    Switch,
-    useParams
-} from "react-router-dom";
+import {HashRouter as Router, Route, Switch, useParams} from "react-router-dom";
 
 export const FeatureItem = () => {
     const {feature, item} = useParams();
