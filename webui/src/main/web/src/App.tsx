a87d58296822ff0518d67c15216315116e06e261
Paul Campbell <pcampbell@kemitix.net>
2020-05-10 12:03:55 +0100

    webui:App: display an error if feature request fails

--- webui/src/main/web/src/App.tsx
+++ webui/src/main/web/src/App.tsx
@@ -9,21 +9,35 @@ import {HashRouter as Router} from "react-router-dom";
 
 const SERVER_URI = "http://localhost:8080/naolo";
 
+const ServerError = () =>
+    <>
+        <h1>Server Error</h1>
+        <br/>
+        <p>Error connecting to server.</p>
+    </>;
+
 const App = () => {
     const [features, setFeatures] = useState([]);
+    const [serverOkay, setServerOkay] = useState(false);//until proven otherwise
     useEffect(() => {
         fetch(SERVER_URI + "/api/ui/features")
             .then(response => response.json())
-            .then(_features => setFeatures(_features));
+            .then(_features => {
+                setFeatures(_features);
+                setServerOkay(true);
+            })
+            .catch(reason => setServerOkay(false));
     }, []);
+    const AppPage = () => {
+        if (serverOkay) return (<><Navigation features={features} serverUri={SERVER_URI}/><MainBody/></>);
+        return <ServerError/>
+    }
     return (
         <Router>
             <div className="App">
                 <Header/>
                 <div className="cf">
-                    <Navigation features={features}
-                                serverUri={SERVER_URI}/>
-                    <MainBody/>
+                    <AppPage/>
                 </div>
                 <Footer/>
             </div>
